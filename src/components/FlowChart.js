6c6cfbdbd569a262a7583830eb9a3be1909adf5b
duynn <ngocduy@tuoitre.com.vn>
2020-12-18 17:22:32 +0700

    update

--- src/components/FlowChart.js
+++ src/components/FlowChart.js
@@ -1,9 +1,9 @@
-import React,{useRef} from 'react';
+import React from 'react';
 import useFlowChart from "../hooks/useFlowChart";
 import './FlowChart.scss';
 
 import Node from "./Node";
-import {FaLock, FaLockOpen, FaSearch, FaSearchMinus, FaSearchPlus} from "react-icons/fa";
+import FlowTool from "./FlowTool";
 
 const FlowChart = ({workflowId=1}) => {
 
@@ -15,25 +15,9 @@ const FlowChart = ({workflowId=1}) => {
         <div className={"flow"}>
             <aside className="flow__sidebar">
                 <Node drag={drag}/>
-                <div><div className="btn-clear" style={{userSelect:'none'}} onClick={()=>editor.clear()}>Xóa</div>
-                    <div className="btn-save" style={{userSelect:'none'}} onClick={()=>{console.log(editor.export())}}>Lưu</div></div>
-                {/*<div className="btn-lock">*/}
-                {/*    {(editor && editor.editor_mode === 'edit') ? <FaLock onClick={() => {*/}
-                {/*            editor.lock();*/}
-                {/*        }}/> :*/}
-                {/*        <FaLockOpen onClick={() => {*/}
-                {/*            editor.unlock();*/}
-                {/*        }}/>*/}
-                {/*    }*/}
-                {/*</div>*/}
-                <div className="bar-zoom">
-                    <FaSearchMinus onClick={() => editor.zoom_out()}/>
-                    <FaSearchPlus onClick={() => editor.zoom_in()}/>
-                    <FaSearch onClick={() => editor.zoom_reset()}/>
-                </div>
+                <FlowTool editor={editor}/>
             </aside>
             <main  id={workflowId} className="flow__draw" onDragOver={allowDrop} onDrop={drop}/>
-
         </div>
 
     );
---
+++ src/components/FlowTool.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {FaSearch, FaSearchMinus, FaSearchPlus} from "react-icons/fa";
+
+const FlowTool = ({editor}) => {
+    return (
+        <div>
+            <div><div className="btn-clear" style={{userSelect:'none'}} onClick={()=>editor.clear()}>Xóa</div>
+                <div className="btn-save" style={{userSelect:'none'}} onClick={()=>{console.log(editor.export())}}>Lưu</div></div>
+            {/*<div className="btn-lock">*/}
+            {/*    {(editor && editor.editor_mode === 'edit') ? <FaLock onClick={() => {*/}
+            {/*            editor.lock();*/}
+            {/*        }}/> :*/}
+            {/*        <FaLockOpen onClick={() => {*/}
+            {/*            editor.unlock();*/}
+            {/*        }}/>*/}
+            {/*    }*/}
+            {/*</div>*/}
+            <div className="bar-zoom">
+                <FaSearchMinus onClick={() => editor.zoom_out()}/>
+                <FaSearchPlus onClick={() => editor.zoom_in()}/>
+                <FaSearch onClick={() => editor.zoom_reset()}/>
+            </div>
+        </div>
+    );
+};
+
+export default FlowTool;
