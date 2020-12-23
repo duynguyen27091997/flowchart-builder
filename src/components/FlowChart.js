c052da50ea7e6f9e73b1379a4716f4bae6e1fa8e
giangnn <giangnguyen.neko.130@gmail.com>
2020-12-23 11:35:37 +0700

    somethingf

--- src/components/FlowChart.js
+++ src/components/FlowChart.js
@@ -15,27 +15,16 @@ const FlowChart = ({workflowId=1}) => {
                 {
                     "step_id": 1,
                     "workflow_id": 1,
-                    "description": "Nhan su tao hs",
-                    "name": "Tao HS1",
-                    "data": {},
-                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Tao HS1</h6>\n              <p>Nhan su tao hs</p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : editorial_board</p>\n                    <p class=\"box__action\">Tác vụ : create</p>\n                    <p class=\"box__check\">\n                       <input type=\"checkbox\" aria-label=\"\" onclick=\"return false;\" checked> <span>first</span>\n                    </p>\n                  </div>\n            </div>\n            ",
+                    "description": "Đề xuất",
+                    "name": "Đề xuất",
+                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Đề xuất</h6>\n              <p>Đề xuất</p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : personal</p>\n                    <p class=\"box__action\">Tác vụ : create</p>\n                  </div>\n            </div>\n            ",
                     "is_first": true,
-                    "inputs": [
-                        {
-                            "name": "input_default",
-                            "steps": [
-                                {
-                                    "step_id": "2",
-                                    "output": "confirm_output_reject"
-                                }
-                            ]
-                        }
-                    ],
+                    "inputs": [],
                     "actions": [
                         {
                             "name": "create",
-                            "target_type": "editorial_board",
-                            "pass": "2",
+                            "target_type": "personal",
+                            "pass": "5",
                             "reject": null
                         }
                     ],
@@ -43,20 +32,19 @@ const FlowChart = ({workflowId=1}) => {
                         {
                             "id": "",
                             "name": "",
-                            "type": "editorial_board",
+                            "type": "personal",
                             "action": "create"
                         }
                     ],
-                    "pos_x": -315,
-                    "pos_y": 427
+                    "pos_x": 17,
+                    "pos_y": 93
                 },
                 {
-                    "step_id": 2,
+                    "step_id": 5,
                     "workflow_id": 1,
-                    "description": "",
-                    "name": "Duyet hs",
-                    "data": {},
-                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Duyet hs</h6>\n              <p></p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : newsroom_secretary</p>\n                    <p class=\"box__action\">Tác vụ : confirm</p>\n                    <p class=\"box__check\">\n                       \n                    </p>\n                  </div>\n            </div>\n            ",
+                    "description": "Trưởng ban",
+                    "name": "Trưởng ban",
+                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Trưởng ban</h6>\n              <p>Trưởng ban</p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : chairman</p>\n                    <p class=\"box__action\">Tác vụ : confirm</p>\n                  </div>\n            </div>\n            ",
                     "is_first": false,
                     "inputs": [
                         {
@@ -72,36 +60,35 @@ const FlowChart = ({workflowId=1}) => {
                     "actions": [
                         {
                             "name": "confirm",
-                            "target_type": "newsroom_secretary",
-                            "pass": "3",
-                            "reject": "1"
+                            "target_type": "chairman",
+                            "pass": "6",
+                            "reject": null
                         }
                     ],
                     "targets": [
                         {
                             "id": "",
                             "name": "",
-                            "type": "newsroom_secretary",
+                            "type": "chairman",
                             "action": "confirm"
                         }
                     ],
-                    "pos_x": 26,
-                    "pos_y": 134
+                    "pos_x": 405,
+                    "pos_y": 83
                 },
                 {
-                    "step_id": 3,
+                    "step_id": 6,
                     "workflow_id": 1,
-                    "description": "",
-                    "name": "Luu HS",
-                    "data": {},
-                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Luu HS</h6>\n              <p></p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : leader</p>\n                    <p class=\"box__action\">Tác vụ : confirm</p>\n                    <p class=\"box__check\">\n                       \n                    </p>\n                  </div>\n            </div>\n            ",
+                    "description": "Văn phòng",
+                    "name": "Văn phòng",
+                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Văn phòng</h6>\n              <p>Văn phòng</p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : office</p>\n                    <p class=\"box__action\">Tác vụ : confirm</p>\n                  </div>\n            </div>\n            ",
                     "is_first": false,
                     "inputs": [
                         {
                             "name": "input_default",
                             "steps": [
                                 {
-                                    "step_id": "2",
+                                    "step_id": "5",
                                     "output": "confirm_output_pass"
                                 }
                             ]
@@ -110,7 +97,81 @@ const FlowChart = ({workflowId=1}) => {
                     "actions": [
                         {
                             "name": "confirm",
-                            "target_type": "leader",
+                            "target_type": "office",
+                            "pass": "7",
+                            "reject": "8"
+                        }
+                    ],
+                    "targets": [
+                        {
+                            "id": "",
+                            "name": "",
+                            "type": "office",
+                            "action": "confirm"
+                        }
+                    ],
+                    "pos_x": 840,
+                    "pos_y": 73
+                },
+                {
+                    "step_id": 7,
+                    "workflow_id": 1,
+                    "description": "Kho",
+                    "name": "Kho",
+                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Kho</h6>\n              <p>Kho</p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : warehouse</p>\n                    <p class=\"box__action\">Tác vụ : confirm</p>\n                  </div>\n            </div>\n            ",
+                    "is_first": false,
+                    "inputs": [
+                        {
+                            "name": "input_default",
+                            "steps": [
+                                {
+                                    "step_id": "6",
+                                    "output": "confirm_output_pass"
+                                }
+                            ]
+                        }
+                    ],
+                    "actions": [
+                        {
+                            "name": "confirm",
+                            "target_type": "warehouse",
+                            "pass": "9",
+                            "reject": null
+                        }
+                    ],
+                    "targets": [
+                        {
+                            "id": "",
+                            "name": "",
+                            "type": "warehouse",
+                            "action": "confirm"
+                        }
+                    ],
+                    "pos_x": 1337,
+                    "pos_y": -90
+                },
+                {
+                    "step_id": 8,
+                    "workflow_id": 1,
+                    "description": "Trưởng ban",
+                    "name": "Trưởng ban",
+                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Trưởng ban</h6>\n              <p>Trưởng ban</p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : chairman</p>\n                    <p class=\"box__action\">Tác vụ : view</p>\n                  </div>\n            </div>\n            ",
+                    "is_first": false,
+                    "inputs": [
+                        {
+                            "name": "input_default",
+                            "steps": [
+                                {
+                                    "step_id": "6",
+                                    "output": "confirm_output_reject"
+                                }
+                            ]
+                        }
+                    ],
+                    "actions": [
+                        {
+                            "name": "view",
+                            "target_type": "chairman",
                             "pass": null,
                             "reject": null
                         }
@@ -119,12 +180,49 @@ const FlowChart = ({workflowId=1}) => {
                         {
                             "id": "",
                             "name": "",
-                            "type": "leader",
+                            "type": "chairman",
+                            "action": "view"
+                        }
+                    ],
+                    "pos_x": 1356,
+                    "pos_y": 264
+                },
+                {
+                    "step_id": 9,
+                    "workflow_id": 1,
+                    "description": "Người đề xuất xuống kho nhận",
+                    "name": "Người đề xuất xuống kho nhận",
+                    "html": "\n            <div>\n              <div class=\"title-box\"><h6>Người đề xuất xuống kho nhận</h6>\n              <p>Người đề xuất xuống kho nhận</p>\n              </div>\n                  <div class=\"box\">\n                    <p class=\"box__target\">Đối tượng : personal</p>\n                    <p class=\"box__action\">Tác vụ : confirm</p>\n                  </div>\n            </div>\n            ",
+                    "is_first": false,
+                    "inputs": [
+                        {
+                            "name": "input_default",
+                            "steps": [
+                                {
+                                    "step_id": "7",
+                                    "output": "confirm_output_pass"
+                                }
+                            ]
+                        }
+                    ],
+                    "actions": [
+                        {
+                            "name": "confirm",
+                            "target_type": "personal",
+                            "pass": null,
+                            "reject": null
+                        }
+                    ],
+                    "targets": [
+                        {
+                            "id": "",
+                            "name": "",
+                            "type": "personal",
                             "action": "confirm"
                         }
                     ],
-                    "pos_x": 406,
-                    "pos_y": 446
+                    "pos_x": 1769,
+                    "pos_y": -113
                 }
             ]
         });
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,24 +1,41 @@
-import React,{useEffect} from 'react';
+import React, {useEffect} from 'react';
 
 import "./Node.scss"
 import {useState} from "react";
 import {FaPlus} from "react-icons/fa";
 import {actions, targets} from "../helpers/helpers";
 
+import Select from 'react-select'
+import AsyncSelect from 'react-select/async';
+import makeAnimated from 'react-select/animated';
 
+const options = [
+    {value: 'chocolate', label: 'Chocolate'},
+    {value: 'strawberry', label: 'Strawberry'},
+    {value: 'vanilla', label: 'Vanilla'}
+]
+const animatedComponents = makeAnimated();
+
+const loadOptions = (inputValue, callback) => {
+    setTimeout(() => {
+        callback(options)
+    }, 3000);
+};
 const Node = ({drag}) => {
         let template = {
             name: '',
             description: '',
             action: '',
             target: '',
-            is_first:false
+            is_first: false
         };
         let [show, setShow] = useState(false)
-        let [value, setValue] = useState(template)
+        let [value, setValue] = useState(template);
+        let [targets, setTargets] = useState([]);
+        let [actions, setActions] = useState([]);
 
         const handleChecked = (e) => {
-            setValue({...value,is_first:e.target.checked})
+            setValue({...value, is_first: e.target.checked})
         }
 
         const handleChange = (e) => {
@@ -29,11 +46,47 @@ const Node = ({drag}) => {
             setValue(template)
         }
 
+        const getData = async () => {
+            const targets = await fetch('http://workflow.chut/api/step/get-action-target-types')
+            const actions = await fetch('http://workflow.chut/api/step/get-action-types')
+            return Promise.all([targets.json(), actions.json()])
+        }
+
+        const renderSelectByTarget = () => {
+            switch (value['target']) {
+                case 'personal': {
+                    return <div className={"node__form-group"}>
+                        <label>Đối tượng cụ thể</label>
+                        <AsyncSelect closeMenuOnSelect={true}
+                                     components={animatedComponents}
+                                     cacheOptions
+                                     isMulti
+                                     loadOptions={loadOptions}/>
+                    </div>
+                }
+            }
+        }
+
+        useEffect(() => {
+            getData().then(res => {
+                setTargets(Object.keys(res[0]).reduce((init, current) => {
+                    return [...init, {value: current, label: res[0][current]}]
+                }, []));
+                setActions(Object.keys(res[1]).reduce((init, current) => {
+                    return [...init, {value: current, label: res[1][current]}]
+                }, []));
+            })
+        }, []);
+
+       const handleSelectChange = (val, action) => {
+           setValue({...value, [action.name]: val.value});
+        };
+
         return (
             <div>
                 {/*<h5>NODE</h5>*/}
                 <div className="node"
-                     draggable={show} onDragEnd={() => reset()} onDragStart={(e)=>drag(value,e)}>
+                     draggable={show} onDragEnd={() => reset()} onDragStart={(e) => drag(value, e)}>
                     {!show ? <div style={{textAlign: "center"}} onClick={() => setShow(true)}><FaPlus/></div> :
                         <div>
                             <div className={"node__form-group"}>
@@ -48,19 +101,20 @@ const Node = ({drag}) => {
                             </div>
                             <div className={"node__form-group"}>
                                 <label>Đối tượng</label>
-                                <select value={value.target} placeholder={'Đối tượng'} onChange={handleChange}
-                                        name={'target'}>
-                                    <option value="" disabled={true}>...</option>
-                                    {targets.map((item, key) => <option key={key} value={item}>{item}</option>)}
-                                </select>
+                                <Select
+                                    onChange={handleSelectChange}
+                                    name={'target'}
+                                    options={targets}
+                                    defaultValue={targets.length > 0 ? targets[0] : null}/>
                             </div>
+                            {renderSelectByTarget()}
                             <div className={"node__form-group"}>
                                 <label>Hành động</label>
-                                <select value={value.action} placeholder={'Hành động'} onChange={handleChange}
-                                        name={'action'}>
-                                    <option value="" disabled={true}>...</option>
-                                    {actions.map((item, key) => <option key={key} value={item}>{item}</option>)}
-                                </select>
+                                <Select
+                                    name={'action'}
+                                    onChange={handleSelectChange}
+                                    options={actions}
+                                    defaultValue={actions.length > 0 ? actions[0] : null}/>
                             </div>
                             <div className={"node__form-group node__form-group-checked"}>
                                 <input type="checkbox" name={'is_first'} value={value.is_first}
