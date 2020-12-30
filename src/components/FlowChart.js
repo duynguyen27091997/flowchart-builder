7fcd33852996ad21d489e51059e48b60c005655c
duynn <ngocduy@tuoitre.com.vn>
2020-12-30 14:49:43 +0700

    update

--- src/components/FlowChart.js
+++ src/components/FlowChart.js
@@ -60,9 +60,41 @@ const FlowChart = ({workflowId = null}) => {
                 return [...init, {value: current, label: res[2][current]}]
             }, []);
 
+            setTargets(targets);
+            setActions(actions);
+            setWorkflowTypes(workflowTypes);
+        })
+    }, []);
 
-    if(editor) {
-        editor.import({"steps":[{"step_id":2,"workflow_id":1,"description":"Phòng nhân sự tạo hồ sơ cho nhân viên","name":"Tạo hồ sơ","html":"<div><div class=\"title-box\"><h6>Tạo hồ sơ</h6><p>Phòng nhân sự tạo hồ sơ cho nhân viên</p></div><div class=\"box\"><p class=\"box__target\">Đối tượng chung: Phòng ban</p><p class=\"box__action\">Tác vụ : Tạo</p><p class=\"box__action\">Đối tượng cụ thể : <br><strong> Leanne Graham</strong></p></div></div>","is_first":true,"inputs":[],"actions":[{"name":"create","target_type":"department","pass":"3","reject":null}],"targets":[{"id":1,"name":"Leanne Graham","type":"department","action":"create"}],"pos_x":123,"pos_y":79},{"step_id":3,"workflow_id":1,"description":"Nhân viên xem & sửa thông tin","name":"Nhân viên cập nhật hồ sơ","html":"<div><div class=\"title-box\"><h6>Nhân viên cập nhật hồ sơ</h6><p>Nhân viên xem & sửa thông tin</p></div><div class=\"box\"><p class=\"box__target\">Đối tượng chung: Cá nhân</p><p class=\"box__action\">Tác vụ : Cập nhật</p><p class=\"box__action\">Đối tượng cụ thể : <br><strong> Leanne Graham</strong></p></div></div>","is_first":false,"inputs":[{"name":"input_default","steps":[{"step_id":"2","output":"create_output_pass"}]}],"actions":[{"name":"update","target_type":"personal","pass":"4","reject":null}],"targets":[{"id":1,"name":"Leanne Graham","type":"personal","action":"update"}],"pos_x":582,"pos_y":109},{"step_id":4,"workflow_id":1,"description":"Phòng nhân sự xem xét, lưu thông tin đã chỉnh sửa từ nhân viên","name":"Phòng Nhân sự lưu hồ sơ","html":"<div><div class=\"title-box\"><h6>Phòng Nhân sự lưu hồ sơ</h6><p>Phòng nhân sự xem xét, lưu thông tin đã chỉnh sửa từ nhân viên</p></div><div class=\"box\"><p class=\"box__target\">Đối tượng chung: Phòng ban</p><p class=\"box__action\">Tác vụ : Xác nhận</p><p class=\"box__action\">Đối tượng cụ thể : <br><strong> Leanne Graham</strong></p></div></div>","is_first":false,"inputs":[{"name":"input_default","steps":[{"step_id":"3","output":"update_output_pass"}]}],"actions":[{"name":"confirm","target_type":"department","pass":null,"reject":null}],"targets":[{"id":1,"name":"Leanne Graham","type":"department"}],"pos_x":965,"pos_y":96}]});
+    const handleSave = steps => {
+        setLoading(true);
+        let data = {
+            workflow_name: workflow.workflow_name,
+            workflow_type: workflow.workflow_type,
+            workflow_description: workflow.workflow_description,
+            steps: steps.steps
+        }
+        setTimeout(() => {
+            axios.post('https://workflow.tuoitre.vn/api/step/store-steps', data).then(
+                res => {
+                    setSwalTitle('Success')
+                    setSwalText('Create workflow successful')
+                    setLoading(false);
+                    setSwal(true);
+                    editor.clear()
+                },
+                err => {
+                    setSwalTitle('Error')
+                    setSwalText('Oops, have an error')
+                    setLoading(false);
+                    setSwal(true);
+                }
+            )
+        }, 2000)
+    }
+
+    if (editor) {
+        editor.start();
     }
 
     const [modalIsOpen, setIsOpen] = React.useState(false);
