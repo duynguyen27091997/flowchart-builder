71afa4c0e04e75493b44082b275a1aff021b11ed
duynn <ngocduy@tuoitre.com.vn>
2021-01-07 09:09:10 +0700

    Updates

---
+++ precache-manifest.7d00051f5bd92e296e529b4ebf9abd08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac0ab02a1afef898f175915d1f4fb4a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76cc0c9e39edd83c7766",
+    "url": "./static/css/main.cd7ba046.chunk.css"
+  },
+  {
+    "revision": "b26655daa910167966d2",
+    "url": "./static/js/2.dd9ab253.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dd9ab253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cc0c9e39edd83c7766",
+    "url": "./static/js/main.91359894.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.91359894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d5a1dbe617a6b2f1a0",
+    "url": "./static/js/runtime-main.7191a349.js"
+  }
+]);
