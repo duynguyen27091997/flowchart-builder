f4744f7646d4a9d5a42e7414c8f7a33a4dbd6f77
giangnn <giangnguyen.neko.130@gmail.com>
2021-03-19 09:28:21 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Flow from './components/Flow'
 
-export const TTFlow = ({urls}) => {
-  return <Flow urls={urls}/>
+export const TTFlow = ({urls, tableId}) => {
+  return <Flow urls={urls} tableId={tableId}/>
 }
